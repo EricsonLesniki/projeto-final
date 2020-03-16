1dc9b06786fdf23c97436a9484a292c059193e8a
Ericson Lesniki <ericsonlesniki@gmail.com>
2020-03-16 11:32:00 -0300

    alterei a index.php

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-  <title>Cadastro de produtos</title>
+  <title>Cadastro</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
